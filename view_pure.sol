fba45bc0bac3cbb1910ab22a7433f834961885f2
web3saiful <web3saiful@gmail.com>
2025-12-02 23:57:33 +0600

    "update: ongoing learning & practice iterations"

--- view_pure.sol
+++ view_pure.sol
@@ -2,10 +2,19 @@
 pragma solidity ^0.8.10;
 
 contract simple_view_pure{
+
+// uint public count;
+
+// function increment() public {
+//     count = count + 1;   // state পরিবর্তন হচ্ছে
+// }
+
+
+
 uint public count;
 
-function increment() public {
-    count = count + 1;   // state পরিবর্তন হচ্ছে
+function getCount() public view returns(uint) {
+    return count;   // শুধু পড়ছে (read-only)
 }
 
 
