6a3de39a8a3b933aa95b25ea67f431f078a8ff49
web3saiful <web3saiful@gmail.com>
2025-12-02 17:55:08 +0600

    "update: ongoing learning & practice iterations"

--- StrgeMemoryCalldata.sol
+++ StrgeMemoryCalldata.sol
@@ -9,7 +9,7 @@ contract DataLocations{
     }
     mapping (address =>MyStruct)public myStructs;
 
-    function examples()external{
+    function examples(uint[] calldata y,string calldata s)external returns (uint[] memory) {
         myStructs[msg.sender]=MyStruct({foo:123,text:"bar"});
 
         MyStruct storage myStruct=myStructs[msg.sender];
@@ -17,5 +17,10 @@ contract DataLocations{
 
         MyStruct memory readOnly=myStructs[msg.sender];
         readOnly.foo=456;
+
+   uint[]memory memArr= new uint[](3);
+   memArr[0]=254;
+   return memArr;
+
     }
 }
