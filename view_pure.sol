f2ebea00c573e8bd9d410036722bc332972c4b90
web3saiful <web3saiful@gmail.com>
2025-12-02 23:58:15 +0600

    "update: ongoing learning & practice iterations"

--- view_pure.sol
+++ view_pure.sol
@@ -11,11 +11,16 @@ contract simple_view_pure{
 
 
 
-uint public count;
+// uint public count;
 
-function getCount() public view returns(uint) {
-    return count;   // শুধু পড়ছে (read-only)
+// function getCount() public view returns(uint) {
+//     return count;   // শুধু পড়ছে (read-only)
+// }
+
+function add(uint a, uint b) public pure returns(uint) {
+    return a + b;   // শুধু হিসাব, কোন স্টেট পড়ছে না বা লিখছে না
 }
+
 
 
 }
