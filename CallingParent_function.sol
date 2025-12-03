b516dd00b601f2ff418f53d033b444008a156cbb
web3saiful <web3saiful@gmail.com>
2025-12-03 16:54:33 +0600

    "update: ongoing learning & practice iterations"

--- CallingParent_function.sol
+++ CallingParent_function.sol
@@ -24,3 +24,33 @@ contract U is S("saiful"),T("coach"){
 
 }
 
+
+// way 2
+contract V is S,T{
+    constructor(string memory _name,string memory _text) S(_name)T(_text){
+
+    }
+}
+// Way 2 with  static declaration
+contract VV is S("s"),T{
+    constructor(string memory _text)T(_text){
+
+    }
+}
+
+
+// Order of execution
+// 1.S
+// 2.T
+// 3.V0
+contract V0 is S,T{
+    constructor(string memory _name,string memory _text) S(_name) T(_text){} //what cames after "is" will always take precedence.
+}
+
+// Order of execution
+// 1.S
+// 2.T
+// 3.V0
+contract V1 is S,T{
+    constructor(string memory _name,string memory _text) T(_text) S(_name){} //what cames after "is" will always take precedence.
+}
