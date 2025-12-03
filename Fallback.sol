6287a559074bc74302f4487daa56c2b625a2f3d7
web3saiful <web3saiful@gmail.com>
2025-12-03 20:04:43 +0600

    "update: ongoing learning & practice iterations"

--- Fallback.sol
+++ Fallback.sol
@@ -2,17 +2,50 @@
 pragma solidity ^0.8.10;
 
 
-contract Fallback{
-                 send Ether
-                      |
-           msg.data is empty?
-                /           \
-            yes             no
-             |                |
-    receive() exists?     fallback()
-        /        \
-     yes          no
-      |            |
-  receive()     fallback()
+// contract Fallback{
+//                  send Ether
+//                       |
+//            msg.data is empty?
+//                 /           \
+//             yes             no
+//              |                |
+//     receive() exists?     fallback()
+//         /        \
+//      yes          no
+//       |            |
+//   receive()     fallback()
+// }
+
+
+
+contract Fallback {
+    event Log(string func, uint256 gas);
+
+    // Fallback function must be declared as external.
+    fallback() external payable {
+        // send / transfer (forwards 2300 gas to this fallback function)
+        // call (forwards all of the gas)
+        emit Log("fallback", gasleft());
+    }
+
+    // Receive is a variant of fallback that is triggered when msg.data is empty
+    receive() external payable {
+        emit Log("receive", gasleft());
+    }
+
+    // Helper function to check the balance of this contract
+    function getBalance() public view returns (uint256) {
+        return address(this).balance;
+    }
 }
 
+contract SendToFallback {
+    function transferToFallback(address payable _to) public payable {
+        _to.transfer(msg.value);
+    }
+
+    function callFallback(address payable _to) public payable {
+        (bool sent,) = _to.call{value: msg.value}("");
+        require(sent, "Failed to send Ether");
+    }
+}
