631a457ba7971dc31be13ae2c71839cd50d9b25f
web3saiful <web3saiful@gmail.com>
2025-12-03 12:12:53 +0600

    "update: ongoing learning & practice iterations"

---
+++ callingParantConstructor.sol
@@ -0,0 +1,30 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+// 2ways to call parent constructors
+// Order of initialization
+
+contract S{
+    string public name;
+
+    constructor(string memory _name){
+        name=_name;
+    }
+}
+contract T{
+    string public text;
+    
+    constructor(string memory _text){
+        text=_text;
+
+    }   
+}
+contract U is S("s"),T("t") {
+  
+}
+contract V is S,T{
+   constructor (string memory _name,string memory _text)S(_name)T(_text){
+
+
+
+
