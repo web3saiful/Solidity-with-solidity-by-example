695e0ded787267144e7d01baef8a030e79a6e5fb
web3saiful <web3saiful@gmail.com>
2025-12-04 22:43:21 +0600

    "update: ongoing learning & practice iterations"

--- function_advance.sol
+++ function_advance.sol
@@ -62,71 +62,99 @@
 
 
 
+// // SPDX-License-Identifier: MIT
+// pragma solidity ^0.8.10;
+
+// /* Inheritance tree
+//    A
+//  /  \
+// B   C
+//  \ /
+//   D
+// */
+
+// contract A {
+//     // This is called an event. You can emit events from your function
+//     // and they are logged into the transaction log.
+//     // In our case, this will be useful for tracing function calls.
+//     event Log(string message);
+
+//     function foo() public virtual {
+//         emit Log("A.foo called");
+//     }
+
+//     function bar() public virtual {
+//         emit Log("A.bar called");
+//     }
+// }
+
+// contract B is A {
+//     function foo() public virtual override {
+//         emit Log("B.foo called");
+//         A.foo();
+//     }
+
+//     function bar() public virtual override {
+//         emit Log("B.bar called");
+//         super.bar();
+//     }
+// }
+
+// contract C is A {
+//     function foo() public virtual override {
+//         emit Log("C.foo called");
+//         A.foo();
+//     }
+
+//     function bar() public virtual override {
+//         emit Log("C.bar called");
+//         super.bar();
+//     }
+// }
+
+// contract D is B, C {
+//     // Try:
+//     // - Call D.foo and check the transaction logs.
+//     //   Although D inherits A, B and C, it only called C and then A.
+//     // - Call D.bar and check the transaction logs
+//     //   D called C, then B, and finally A.
+//     //   Although super was called twice (by B and C) it only called A once.
+
+//     function foo() public override(B, C) {
+//         super.foo();
+//     }
+
+//     function bar() public override(B, C) {
+//         super.bar();
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
 
-/* Inheritance tree
-   A
- /  \
-B   C
- \ /
-  D
-*/
 
-contract A {
-    // This is called an event. You can emit events from your function
-    // and they are logged into the transaction log.
-    // In our case, this will be useful for tracing function calls.
-    event Log(string message);
+contract Receive{
+    event Received(address caller,uint amount,string message);
 
-    function foo() public virtual {
-        emit Log("A.foo called");
-    }
+    fallback() external payable{
+        emit Received(msg.sender,msg.value,"Fallback was called");
+     }
+     function foo(string memory _message,uint _x)public payable returns(uint){
+        emit Received(msg.sender,msg.value,_message);
 
-    function bar() public virtual {
-        emit Log("A.bar called");
-    }
+        return _x+1;
+     }
 }
-
-contract B is A {
-    function foo() public virtual override {
-        emit Log("B.foo called");
-        A.foo();
-    }
-
-    function bar() public virtual override {
-        emit Log("B.bar called");
-        super.bar();
-    }
-}
-
-contract C is A {
-    function foo() public virtual override {
-        emit Log("C.foo called");
-        A.foo();
-    }
-
-    function bar() public virtual override {
-        emit Log("C.bar called");
-        super.bar();
-    }
-}
-
-contract D is B, C {
-    // Try:
-    // - Call D.foo and check the transaction logs.
-    //   Although D inherits A, B and C, it only called C and then A.
-    // - Call D.bar and check the transaction logs
-    //   D called C, then B, and finally A.
-    //   Although super was called twice (by B and C) it only called A once.
-
-    function foo() public override(B, C) {
-        super.foo();
-    }
-
-    function bar() public override(B, C) {
-        super.bar();
-    }
-}
-
-
