63acd06c862dbd076a229a84902377d3480011ba
web3saiful <web3saiful@gmail.com>
2025-12-05 11:37:53 +0600

    "update: ongoing learning & practice iterations"

--- Libarary.sol
+++ Libarary.sol
@@ -14,22 +14,22 @@ contract Test{
 }
 
 
-//  library ArrayLib{
-//     function find(uint[] storage arr,uint x)internal view returns(uint){
-//         for (uint i=0;i<arr.length;i++){
-//             if (arr[i]==x){
-//                 return i;
-//             }
-//         }
-//         revert("not found");
-//     }
-//  }
-// contract TestArray{
-//     using ArrayLib for uint[];
-//     uint[]public arr=[3,2,1];
+ library ArrayLib{
+    function find(uint[] storage arr,uint x)internal view returns(uint){
+        for (uint i=0;i<arr.length;i++){
+            if (arr[i]==x){
+                return i;
+            }
+        }
+        revert("not found");
+    }
+ }
+contract TestArray{
+    using ArrayLib for uint[];
+    uint[]public arr=[3,2,1];
 
-//     function testFind()external view returns(uint i){
-//     //   return ArrayLib.find(arr,2);
-//       return arr.find(2);
-//     }
-// }
+    function testFind()external view returns(uint i){
+    //   return ArrayLib.find(arr,2);
+      return arr.find(2);
+    }
+}
