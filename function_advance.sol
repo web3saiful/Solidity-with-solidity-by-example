e7fd268fb8fc2f669fd1c4cc6412f2d5e90b12e6
Saiful Islam <web3saiful@gmail.com>
2025-12-25 22:36:11 +0600

    Update function_advance.sol

--- function_advance.sol
+++ function_advance.sol
@@ -158,3 +158,32 @@ contract Receive{
         return _x+1;
      }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.10;
+
+
+contract Receive{
+    event Received(address caller,uint amount,string message);
+
+    fallback() external payable{
+        emit Received(msg.sender,msg.value,"Fallback was called");
+     }
+     function foo(string memory _message,uint _x)public payable returns(uint){
+        emit Received(msg.sender,msg.value,_message);
+
+        return _x+1;
+     }
+}
