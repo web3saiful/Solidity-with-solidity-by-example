f8376aedd71f4c282c5d1cf1e56bbe505c4f5dfb
Saiful Islam <web3saiful@gmail.com>
2025-12-27 21:36:49 +0600

    Update Immutable.sol

--- Immutable.sol
+++ Immutable.sol
@@ -13,6 +13,18 @@ contract Immutable {
 
 
 
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.26;
+
+contract Immutable {
+    address public immutable myAddr;
+    uint256 public immutable myUint;
+
+    constructor(uint256 _myUint) {
+        myAddr = msg.sender;
+        myUint = _myUint;
+    }
+}
 
 
 
