37f3013f71c1959944238e0aaaec77752fa289f5
Saiful Islam <web3saiful@gmail.com>
2026-01-06 21:51:40 +0600

    Update constant.sol

--- constant.sol
+++ constant.sol
@@ -8,3 +8,14 @@ contract Constants {
         0x777788889999AaAAbBbbCcccddDdeeeEfFFfCcCc;
     uint256 public constant MY_UINT = 123;
 }
+
+
+
+
+
+contract Constants {
+    // coding convention to uppercase constant variables
+    address public constant MY_ADDRESS =
+        0x777788889999AaAAbBbbCcccddDdeeeEfFFfCcCc;
+    uint256 public constant MY_UINT = 123;
+}
