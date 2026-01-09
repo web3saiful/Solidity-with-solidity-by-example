2638aede5543333806a5d614884378cdb2cff348
Saiful Islam <web3saiful@gmail.com>
2026-01-09 23:57:05 +0600

    Update gas.sol

--- gas.sol
+++ gas.sol
@@ -49,3 +49,20 @@ contract Gas {
 
 
 
+
+
+
+contract Gas {
+    uint256 public i = 0;
+
+    // Using up all of the gas that you send causes your transaction to fail.
+    // State changes are undone.
+    // Gas spent is not refunded.
+    function forever() public {
+        // Here we run a loop until all of the gas are spent
+        // and the transaction fails
+        while (true) {
+            i += 1;
+        }
+    }
+}
