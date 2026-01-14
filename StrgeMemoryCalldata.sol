c9425e4da28a3b8b54eab9d03f6ac9132ac8781c
Saiful Islam <web3saiful@gmail.com>
2026-01-14 21:51:33 +0600

    "update: ongoing learning & practice iterations"

--- StrgeMemoryCalldata.sol
+++ StrgeMemoryCalldata.sol
@@ -56,3 +56,39 @@ contract DataLocations{
 
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+contract DataLocations{
+    struct MyStruct{
+        uint foo;
+        string text;
+    }
+    mapping (address =>MyStruct)public myStructs;
+
+    function examples(uint[] calldata y,string calldata s)external returns (uint[] memory) {
+        myStructs[msg.sender]=MyStruct({foo:123,text:"bar"});
+
+        MyStruct storage myStruct=myStructs[msg.sender];
+        myStruct.text="foo";
+
+        MyStruct memory readOnly=myStructs[msg.sender];
+        readOnly.foo=456;
+
+   uint[]memory memArr= new uint[](3);
+   memArr[0]=254;
+   return memArr;
+
+    }
+}
+
