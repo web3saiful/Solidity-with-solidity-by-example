24f2747714b0b4140e06462e882784ddf87c4388
Saiful Islam <web3saiful@gmail.com>
2026-01-18 22:27:51 +0600

    Refactor VerifySignature contract structure

--- VerifySignature.sol
+++ VerifySignature.sol
@@ -27,4 +27,34 @@ contract VerifySing{
 
     
     
+
+}
+
+
+
+
+
+
+contract VerifySing{
+    function verify(address _signer,string memory _message,bytes memory _sig)
+    external pure returns(bool)
+    {
+        bytes messageHash=  getMessageHash(_message);
+        bytes ethSignedMessageHash=getEthSignedMessageHash(messageHash);
+
+        return recover(ethSignedMessageHash,_sig)==_signer;
+    }
+
+    function getMessageHash(string memory _message)public pure returns(bytes32){
+        return keccak256(abi.encodePacked(_message));
+    }
+
+    function getEthSignedMessageHash(bytes32 _messageHash)public pure returns(bytes32){
+        return keccak256(abi.encodePacked(
+            "\x19Ethereum Singed Message:\n32",
+            _messageHash));
+    }
+
+    
+    
 }
