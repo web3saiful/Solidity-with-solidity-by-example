83dbf6e260bb6b2a88bd4bc6e473ea0869ccfbd7
Saiful Islam <web3saiful@gmail.com>
2026-01-22 22:00:38 +0600

    "update: ongoing learning & practice iterations"

--- Libarary.sol
+++ Libarary.sol
@@ -32,4 +32,49 @@ contract TestArray{
     //   return ArrayLib.find(arr,2);
       return arr.find(2);
     }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+library Math{
+    function max(uint x,uint y)internal pure returns(uint){
+        return x>=y?x:y;
+    }
+}
+
+contract Test{
+    function testMax(uint x,uint y)external pure returns(uint){
+        return Math.max(x,y);
+    }
+}
+
+
+ library ArrayLib{
+    function find(uint[] storage arr,uint x)internal view returns(uint){
+        for (uint i=0;i<arr.length;i++){
+            if (arr[i]==x){
+                return i;
+            }
+        }
+        revert("not found");
+    }
+ }
+contract TestArray{
+    using ArrayLib for uint[];
+    uint[]public arr=[3,2,1];
+
+    function testFind()external view returns(uint i){
+    //   return ArrayLib.find(arr,2);
+      return arr.find(2);
+    }
 }
