31b34bf3d108e5fe31b093e2f616575aa309d877
Saiful Islam <web3saiful@gmail.com>
2026-01-23 21:51:55 +0600

    "update: ongoing learning & practice iterations"

--- view_pure.sol
+++ view_pure.sol
@@ -23,4 +23,43 @@ function add(uint a, uint b) public pure returns(uint) {
 
 
 
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+contract simple_view_pure{
+
+// uint public count;
+
+// function increment() public {
+//     count = count + 1;   // state পরিবর্তন হচ্ছে
+// }
+
+
+
+// uint public count;
+
+// function getCount() public view returns(uint) {
+//     return count;   // শুধু পড়ছে (read-only)
+// }
+
+function add(uint a, uint b) public pure returns(uint) {
+    return a + b;   // শুধু হিসাব, কোন স্টেট পড়ছে না বা লিখছে না
+}
+
+
+
+
 }
